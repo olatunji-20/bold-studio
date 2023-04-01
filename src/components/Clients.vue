56c55fe59e78f5bbbf6a040c99f518539f0f4116
falana sheriff <falanasheriffdeen@gmail.com>
2023-04-01 09:13:12 +0100

    bold-studio landing page

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -3,7 +3,15 @@
         <div class="main">
             <p>CLIENTS</p>
             <div class="view"></div>
-            <div class="clients"></div>
+            <div class="clients">
+                <div class="roller">
+                    <div class="spons"></div>
+                    <div class="spons"></div>
+                    <div class="spons"></div>
+                    <div class="spons"></div>
+                    <div class="spons"></div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -22,10 +30,11 @@
 <style scoped>
 .main {
     border: 2px solid peru;
-    width: 100%;
+    width: 93%;
     height: 500px;
     text-align: left;
-    padding: 0px 40px;
+    padding-bottom: 50px;
+    margin: 50px auto 0px;
 }
 .main p {
     color: red;
@@ -35,7 +44,7 @@
 .main .view {
     border: 2px solid fuchsia;
     height: 50px;
-    width: 250px;
+    width: 200px;
     /* display: inline-block; */
     float: right;
     margin-top: 40px;
@@ -43,7 +52,23 @@
 .clients {
     border: 1px solid blueviolet;
     width: 100%;
-    height: 250px;
+    height: auto;
     margin: 20px auto;
+    padding: 20px;
+    overflow: auto;
+}
+.clients .roller {
+    border: 2px solid red;
+    height: auto;
+    width: 1500px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    overflow: hidden;
+}
+.clients .spons {
+    border: 1px solid black;
+    height: 250px;
+    width: 250px;
 }
 </style>
